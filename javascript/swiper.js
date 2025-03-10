5b89d404fb3c7a17be272cbe59a6045dd3122f74
Ibrahim Can ONDER <ibrahimcanonder_98@hotmail.com>
2025-03-10 23:27:13 +0000

    web project

---
+++ javascript/swiper.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper(".slider_wrapper", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
