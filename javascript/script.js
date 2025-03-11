bbeb9f9107bdc2d65addaef42f6bf984527c21b7
Ibrahim Can ONDER <ibrahimcanonder_98@hotmail.com>
2025-03-11 22:13:41 +0000

    update html css

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const logsing = document.querySelector(".logsing-menu");
+const account = document.querySelector(".account");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+});
+
+account.addEventListener("click", () => {
+  account.classList.toggle("active");
+  logsing.classList.toggle("active");
 });
 
 document.querySelectorAll(".nav-link").forEach((n) =>
@@ -11,4 +18,5 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
   })
+  
 );
